806d0f7da5dda0557624e1bcb16c7fdfea35f299
RUOPENG Jiang <543068858@qq.com>
2020-05-04 21:00:19 +0100

    邮箱功能

--- community/src/test/java/com/nowcoder/community/MapperTests.java
+++ community/src/test/java/com/nowcoder/community/MapperTests.java
@@ -66,12 +66,12 @@ public class MapperTests {
 
     @Test
     public void testSelectPosts() {
-        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(0, 0, 10);
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
         for(DiscussPost post : list) {
             System.out.println(post);
         }
 
-        int rows = discussPostMapper.selectDiscussPostRows(0);
+        int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
     }
 
